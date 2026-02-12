879c7b2bfcf8b0b5bf38a0a143f893abc09ab76d
Philip Riber Johnsen <philiprjohnsen@gmail.com>
2026-02-12 14:27:34 +0100

    Create no-eyetracking-render-loop.py

---
+++ render/no-eyetracking-render-loop.py
@@ -0,0 +1,29 @@
+#This file is a rendering loop that does not utilise eye tracking, thus no masking is done and the gaussian filtering of Swiatzcak and Schaeffel is used throughout
+
+#Notes for this:
+#edges are calculated as repeating the edge pixel
+#the kernel used can be either gaussian or circular
+
+#Process / list of actions
+  #Import files and libraries
+
+  #Load settings into memory
+
+  #Initialize frame capture
+
+  #Initialize render_blur.py
+
+  #Initialize rendering loop
+    #Capture a frame
+
+    #Blur the frame
+
+    #display the frame
+
+    #option for quitting the rendering loop
+
+    #telemetry / debug prints
+
+  #cleanup when quitting
+
+
